846a5b28ee9556e1046abfd780d01e4500e47efe
sglord <lordstepheng@gmail.com>
2019-02-21 13:49:23 -0500

    removed files, created clothing model

--- client/app.js
+++ client/app.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import {Navbar} from './components'
-import Routes from './routes'
+import { Navbar } from './components';
+import Routes from './routes';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+			<Routes />
+		</div>
+	);
+};
 
-export default App
+export default App;
